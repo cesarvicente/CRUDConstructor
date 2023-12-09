863966385fe4eefc1ce9d62dba875f93246363e0
Cesar Vicente <cesar_vicente@outlook.com.br>
2023-12-09 00:11:44 -0300

    arquitetura base do Projeto MVC

--- IndexPage.Designer.cs
+++ IndexPage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CRUDConstructor
 {
-    partial class Form1
+    partial class IndexPage
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // IndexPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(663, 463);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "IndexPage";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CRUD Constructor";
+            ResumeLayout(false);
         }
 
         #endregion
--- IndexPage.cs
+++ IndexPage.cs
@@ -1,8 +1,8 @@
 namespace CRUDConstructor
 {
-    public partial class Form1 : Form
+    public partial class IndexPage : Form
     {
-        public Form1()
+        public IndexPage()
         {
             InitializeComponent();
         }
