54b1131ad7e5b7f9ed38c9d8d77ec539a3f3b148
cesarvicente <cesar@sastec.com.br>
2023-12-12 08:27:45 -0300

    method getListTables and DAO, Controller

---
+++ Controller/Connection.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDConstructor.Controller
+{
+    public class Connection
+    {
+        private MySqlConnection conn;
+        private DataAccessObject.Connection data;
+
+        public Connection(MySqlConnection _conn)
+        {
+            conn = _conn;
+            data = new DataAccessObject.Connection(conn);
+        }
+
+        public List<string> getListTables() => data.getTablesList();
+    }
+}
---
+++ DataAccessObject/Connection.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDConstructor.DataAccessObject
+{
+    internal class Connection
+    {
+        private MySqlConnection conn;
+        public Connection(MySqlConnection _conn)
+        {
+            conn = _conn;
+        }
+
+        internal List<string> getTablesList()
+        {
+            var listTables = new List<string>();
+
+            using(MySqlConnection conn = this.conn)
+            {
+                using(var command = new MySqlCommand())
+                {
+                    command.Connection = conn;
+                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = "SHOW TABLES";
+
+                    try
+                    {
+                        conn.Open();
+                        MySqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            listTables.Add(reader.GetString(0));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                    return listTables;
+                }
+            }
+        }
+
+    }
+}
