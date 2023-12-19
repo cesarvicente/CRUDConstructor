466c8f0860b8c1edb4d6a503e1531157f3e134d9
cesarvicente <cesar@sastec.com.br>
2023-12-19 08:38:44 -0300

    Fix acidental commit

--- Model/DataBaseTable.cs
+++ Model/DataBaseTable.cs
@@ -10,9 +10,67 @@ namespace CRUDConstructor.Model
     {
         public string Field { get; set; }
 
+        private string _typeFormat;
+
+        public string DbType
+        {
+            get { return _typeFormat; }
+            set { _typeFormat = value?.ToUpper(); }
+        }
+
+        public Type codeType { get; set; }
+
         public bool Nullable { get; set; }
 
         public string Key { get; set; }
+
+        public string DefaultValue { get; set; }
+
+        public string ExtraArguments { get; set; }
+
+        public override string ToString()
+        {
+            string text = $"{Field} || {DbType.ToUpper()} {ExtraArguments.ToUpper()}";
+            return text;
+        }
+
+        public void getCodeType()
+        {
+            switch (DbType)
+            {
+                case string s when s.Contains("VARCHAR"):
+                    codeType = typeof(string);
+                    break;
+
+                case string s when s.Contains("INT"):
+                    codeType = typeof(int);
+                    break;
+
+                case string s when s.Contains("DOUBLE"):
+                    codeType = typeof(double);
+                    break;
+
+                case string s when s.Contains("DECIMAL"):
+                    codeType = typeof(decimal);
+                    break;
+
+                case string s when s.Contains("BOOLEAN"):
+                    codeType = typeof(bool);
+                    break;
+
+                case string s when s.Contains("DATE"):
+                    codeType = typeof(DateTime);
+                    break;
+
+                case string s when s.Contains("BLOB"):
+                    codeType = typeof(byte[]);
+                    break;
+
+                default:
+                    codeType = typeof(string);
+                    break;
+            }
+        }
 
     }
 }
